4a71c8400514cae6456c37de7487c72deb87feb8
Vaan <keosphere@hotmail.com>
2018-05-19 21:24:56 +0800

    -

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
+
+import Layout from '../components/MainLayout.js'
+
+
 export default () => (
   <div>
+  <Layout>
     <p>This is the about page</p>
+    </Layout>
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 
-import Header from '../components/Header'
+import Layout from '../components/MainLayout.js'
 
 
 
@@ -7,8 +7,9 @@ import Header from '../components/Header'
 
 const Index = () => (
   <div>
-       <Header />
-    <p>Hello Next.js</p>
+     <Layout>
+       <p>Hello Next.js</p>
+    </Layout>
   </div>
 )
 
