873d921c255fd12660a1157d17b5407250bcb1fa
Vaan <keosphere@hotmail.com>
2019-09-10 18:00:37 +0200

    -

---
+++ pages/about.jsx
@@ -0,0 +1,7 @@
+import Layout from '../components/MainLayout.jsx'
+
+export default() => (<div>
+  <Layout>
+    <p>This is the about page</p>
+  </Layout>
+</div>)
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../components/MainLayout.jsx'
+
+const Index = () => (<div>
+  <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
+</div>)
+
+export default Index
