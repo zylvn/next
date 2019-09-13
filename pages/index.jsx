0e35bab804695407c7748389ec03c12450e3c13e
Vaan <keosphere@hotmail.com>
2019-09-13 21:24:09 +0200

    -

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,28 @@
-import "../style.css"
-import "../style.scss"
+import "../style.css";
+import "../style.scss";
+import fetch from 'isomorphic-unfetch'
 
 import Layout from '../components/MainLayout.jsx'
 
-const Index = () => (<div>
+const Index = (props) => {
+
+console.log(props);
+
+return (
+  <div>
   <Layout>
     <p className="example">Hello Next.js</p>
   </Layout>
-</div>)
+</div>)};
+
+
+Index.getInitialProps = async ()=>{
+const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+console.log(data);
+const result = await data.json();
+console.log(result);
+return{result}
+};
+
 
 export default Index
